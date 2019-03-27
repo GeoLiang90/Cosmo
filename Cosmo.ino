fd01a66bd7cb8cbc2fa391cb09e132972d98bf5d
GeoLiang90 <gliang90@stuy.edu>
2019-03-27 12:39:59 -0400

    Change conventions and fix errors

--- Cosmo.ino
+++ Cosmo.ino
@@ -1,7 +1,5 @@
 #include <PS4BT.h>
 #include <usbhub.h>
-#include <math.h>
-#include <Adafruit_MotorShield.h>
 
 // Satisfy the IDE, which needs to see the include statment in the ino too.
 #ifdef dobogusinclude
@@ -13,15 +11,11 @@ USB Usb;
 //USBHub Hub1(&Usb); // Some dongles have a hub inside
 //& Means the address of 
 BTD Btd(&Usb); // You have to create the Bluetooth Dongle instance like so
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();\
-//Creating 4 motors
-Adafruit_DCMotor *motor1 = AFMS.getMotor(1);
-Adafruit_DCMotor *motor2 = AFMS.getMotor(2);
-Adafruit_DCMotor *motor3 = AFMS.getMotor(3);
-Adafruit_DCMotor *motor4 = AFMS.getMotor(4);
+//Instance of PS4BT
+PS4BT PS4(&Btd, PAIR);
 
 void setup(){
-  AFMS.begin();
+  setup_drivebase();
   Serial.begin(115200);
 #if !defined(__MIPSEL__)
   while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
@@ -37,34 +31,20 @@ void loop(){
   Usb.Task();
   //Preliminary stuff
   if (PS4.connected()){
-    leftSpeed = 0;
-    rightSpeed = 0;
+    int left_speed = 0;
+    int right_speed = 0;
     if (PS4.getAnalogHat(LeftHatY)> 128 || PS4.getAnalogHat(LeftHatY) < 128){
-       leftSpeed = PS4.getAnalogHat(LeftHatY);
+       left_speed = scale_joystick(PS4.getAnalogHat(LeftHatY));
     }
     if (PS4.getAnalogHat(RightHatY)> 128 || PS4.getAnalogHat(RightHatY) < 128){
-      rightSpeed = PS4.getAnalogHat(RightHatY);
+      right_speed = scale_joystick(PS4.getAnalogHat(RightHatY));
     }
-    tankDrive(leftSpeed,rightSpeed);
-    if (leftSpeed > 0 || rightSpeed > 0){
+    tank_drive(left_speed,right_speed);
+    if (left_speed > 0 || right_speed > 0){
       Serial.print(F("\r\nDriving"));
     }
     else{
       Serial.print(F("\r\nStationary"));
     }
   }
-}
-
-int scaleJoystick(int joystickValue){
-  double scale = joystickValue/255;
-  scale = pow(scale,2);
-  return joystickValue * scale;
-}
-
-void tankDrive(int leftStickY, int rightStickY){
-  //Set default speeds of motor 
-  motor1 -> setSpeed(leftStickY);
-  motor2 -> setSpeed(leftStickY);
-  motor3 -> setSpeed(rightStickY);
-  motor4 -> setSpeed(rightStickY);
 }
